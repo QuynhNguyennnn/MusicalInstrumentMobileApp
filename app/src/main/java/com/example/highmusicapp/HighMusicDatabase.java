aa4a4a67d81383db5cf49ea02771579213722e0d
phucnt <phucnt@kdigi.app>
2023-07-11 10:54:51 +0700

    Update Function auto import new database (Not working)

--- app/src/main/java/com/example/highmusicapp/HighMusicDatabase.java
+++ app/src/main/java/com/example/highmusicapp/HighMusicDatabase.java
@@ -1,6 +1,10 @@
 package com.example.highmusicapp;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
@@ -17,21 +21,65 @@ import com.example.highmusicapp.Models.Account;
 import com.example.highmusicapp.Models.Category;
 import com.example.highmusicapp.Models.Product;
 
-@Database(entities = {Product.class, Category.class, Account.class}, version = 9)
-public abstract  class HighMusicDatabase extends RoomDatabase  {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+@Database(entities = {Product.class, Category.class, Account.class}, version = 12)
+public abstract class HighMusicDatabase extends RoomDatabase {
     public abstract ProductDAO getProductDAO();
     public abstract CategoryDAO getCategoryDAO();
     public abstract AccountDAO getAccountDAO();
 
-    public static HighMusicDatabase INSTANCE;
+    private static final String DATABASE_NAME = "external_database.db";
+
+    private static HighMusicDatabase INSTANCE;
 
     public static HighMusicDatabase getInstance(Context context) {
         if (INSTANCE == null) {
+            // Check if the external database exists
+            boolean externalDatabaseExists = checkExternalDatabaseExists(context);
+            if (externalDatabaseExists) {
+                // Import the external database if it exists
+                importExternalDatabase(context);
+            }
+
             INSTANCE = Room.databaseBuilder(context, HighMusicDatabase.class, "HighMusicDatabase")
                     .allowMainThreadQueries()
                     .fallbackToDestructiveMigration()
                     .build();
         }
         return INSTANCE;
+    }
+
+    private static boolean checkExternalDatabaseExists(Context context) {
+        File externalDbFile = context.getDatabasePath(DATABASE_NAME);
+        Log.d("Database Exists", externalDbFile.exists() + "");
+        return externalDbFile.exists();
+    }
+
+    private static void importExternalDatabase(Context context) {
+        File externalDbFile = context.getDatabasePath(DATABASE_NAME);
+
+        Log.d("External DB Path", externalDbFile.toString());
+
+        if (!externalDbFile.getParentFile().exists()) {
+            externalDbFile.getParentFile().mkdirs();
+        }
+
+        try (InputStream inputStream = context.getAssets().open(DATABASE_NAME);
+             OutputStream outputStream = new FileOutputStream(externalDbFile)) {
+
+            byte[] buffer = new byte[8192];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
